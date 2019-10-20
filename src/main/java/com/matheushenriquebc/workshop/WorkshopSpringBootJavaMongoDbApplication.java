bd4c9cd93c6ae5013412016cbe51ae2fc62ba5ba
Matheus Henrique <matheushenrique.dev@gmail.com>
2019-10-20 09:41:55 -0300

    projeto add

---
+++ src/main/java/com/matheushenriquebc/workshop/WorkshopSpringBootJavaMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.matheushenriquebc.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootJavaMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootJavaMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheushenriquebc/workshop/WorkshopSpringBootJavaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushenriquebc.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootJavaMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
